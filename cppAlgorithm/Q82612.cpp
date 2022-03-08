e656f02d951f268d8cd7348f7e48cc6d0bc9b306
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2022-03-08 19:27:24 +0900

    알고리즘 문제 풀이(22.03.08)
    - 프로그래머스 알고리즘 문제 풀이

--- cppAlgorithm/Q82612.cpp
+++ cppAlgorithm/Q82612.cpp
@@ -1,1 +1,9 @@
 // 부족한 금액 계산하기: https://programmers.co.kr/learn/courses/30/lessons/82612
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long cost = 0; int i;
+    for (i = 1; i <= count; i++) cost += price * i;
+    return cost > money ? cost - money : 0;
+}
---
+++ cppAlgorithm/Q86051.cpp
@@ -0,0 +1,8 @@
+// 없는 숫자 더하기: https://programmers.co.kr/learn/courses/30/lessons/86051
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> numbers) {
+
+}
